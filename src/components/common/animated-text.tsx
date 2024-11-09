f3d4950c076ccebc0392fafc5333097fcc6e8bc3
Ripdiegozz <diegogarciag63@gmail.com>
2024-11-09 14:28:21 -0500

    Add initial layout, components, and Tailwind CSS configuration

---
+++ src/components/common/animated-text.tsx
@@ -0,0 +1,115 @@
+import { useEffect, useRef } from "react";
+import { motion, useInView, useAnimation, type Variant } from "framer-motion";
+
+const defaultAnimations = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+    filter: "blur(4px)",
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    filter: "blur(0px)",
+    transition: {
+      duration: 0.1,
+    },
+  },
+};
+
+type AnimatedTextProps = {
+  text: string | string[];
+  el?: keyof JSX.IntrinsicElements;
+  className?: string;
+  once?: boolean;
+  repeatDelay?: number;
+  animation?: {
+    hidden: Variant;
+    visible: Variant;
+  };
+};
+
+export const AnimatedText = ({
+  text,
+  el: Wrapper = "p",
+  className,
+  once,
+  repeatDelay,
+  animation = defaultAnimations,
+}: AnimatedTextProps) => {
+  const controls = useAnimation();
+  const textArray = Array.isArray(text) ? text : [text];
+  const ref = useRef(null);
+  const isInView = useInView(ref, { amount: 0.5, once });
+
+  useEffect(() => {
+    let timeout: any;
+
+    const show = () => {
+      controls.start("visible");
+      if (repeatDelay) {
+        timeout = setTimeout(async () => {
+          await controls.start("hidden");
+          controls.start("visible");
+        }, repeatDelay);
+      }
+    };
+
+    if (isInView) {
+      show();
+    } else {
+      controls.start("hidden");
+    }
+
+    return () => clearTimeout(timeout);
+  }, [isInView]);
+
+  return (
+    <Wrapper className={className}>
+      <span className="sr-only">{textArray.join(" ")}</span>
+      <motion.span
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={{
+          visible: { transition: { staggerChildren: 0.1 } },
+          hidden: {},
+        }}
+        aria-hidden
+      >
+        {textArray.map((line, lineIndex) => (
+          <span className="block" key={`${line}-${lineIndex}`}>
+            {line.split(" ").map((word, wordIndex) => (
+              <motion.span
+                className="inline-block"
+                key={`${word}-${wordIndex}`}
+                variants={{
+                  hidden: { opacity: 0, x: 10, filter: "blur(4px)" },
+                  visible: {
+                    opacity: 1,
+                    x: 0,
+                    filter: "blur(0px)",
+                    transition: { delay: wordIndex * 0.1, duration: 0.2 },
+                  },
+                }}
+              >
+                {word.split("").map((char, charIndex) => (
+                  <motion.span
+                    key={`${char}-${charIndex}`}
+                    className="inline-block"
+                    variants={animation}
+                  >
+                    {char}
+                  </motion.span>
+                ))}
+                <span className="inline-block">&nbsp;</span>
+              </motion.span>
+            ))}
+          </span>
+        ))}
+      </motion.span>
+    </Wrapper>
+  );
+};
+
+export default AnimatedText;
