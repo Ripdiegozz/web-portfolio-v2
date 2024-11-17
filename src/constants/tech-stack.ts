ba6b478c9114fcc56e3d1e4715a8c4b41bc673b3
Ripdiegozz <diegogarciag63@gmail.com>
2024-11-17 18:15:36 -0500

    Refactor Skills component to use external tech stack constant; improve layout styling

---
+++ src/constants/tech-stack.ts
@@ -0,0 +1,101 @@
+const techStack = [
+  {
+    name: "TypeScript",
+    key: "typescript",
+  },
+  {
+    name: "HTML",
+    key: "html",
+  },
+  {
+    name: "CSS",
+    key: "css",
+  },
+  {
+    name: "JavaScript",
+    key: "js",
+  },
+  {
+    name: "Node.js",
+    key: "nodejs",
+  },
+  {
+    name: "Java",
+    key: "java",
+  },
+  {
+    name: "Spring Boot",
+    key: "spring",
+  },
+  {
+    name: "Hibernate",
+    key: "hibernate",
+  },
+  {
+    name: "MongoDB",
+    key: "mongodb",
+  },
+  {
+    name: "Docker",
+    key: "docker",
+  },
+  {
+    name: "Angular",
+    key: "angular",
+  },
+  {
+    name: "Express.js",
+    key: "express",
+  },
+  {
+    name: "TailwindCSS",
+    key: "tailwindcss",
+  },
+  {
+    name: "React.js",
+    key: "react",
+  },
+  {
+    name: "Next.js",
+    key: "nextjs",
+  },
+  {
+    name: "MySQL",
+    key: "mysql",
+  },
+  {
+    name: "PostgreSQL",
+    key: "postgresql",
+  },
+
+  {
+    name: "Prisma ORM",
+    key: "prisma",
+  },
+  {
+    name: "Sequelize ORM",
+    key: "sequelize",
+  },
+  {
+    name: "Azure",
+    key: "azure",
+  },
+  {
+    name: "Vite",
+    key: "vite",
+  },
+  {
+    name: "git",
+    key: "git",
+  },
+  {
+    name: "GitHub",
+    key: "github",
+  },
+  {
+    name: "GitLab",
+    key: "gitlab",
+  },
+];
+
+export default techStack;
